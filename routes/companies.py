591de5411958d63fce9c177e9bb78eff4e9f67bc
aaronpq <aperezq@student.universidadunie.com>
2026-05-08 17:29:31 +0200

    Fix: SQLi - Registrar empresa

--- routes/companies.py
+++ routes/companies.py
@@ -104,7 +104,8 @@ def register_company():
         description = request.form['description']
         owner = request.form.get('owner', session.get('username'))
         conn = get_data_connection()
-        conn.execute("INSERT INTO companies (name, description, owner) VALUES ("+company_name+", '"+description+"', '"+owner+"')")
+        query = "INSERT INTO companies (name, description, owner) VALUES (?, ?, ?)"
+	conn.execute(query, (company_name, description, owner))
         conn.commit()
         conn.close()
         flash("Company registered successfully.", "success")
