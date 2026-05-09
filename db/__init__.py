00f8e316eb73ab8864a7d5dbdf2946b82fbe2f11
aaronpq <aperezq@student.universidadunie.com>
2026-05-09 22:53:19 +0200

    Fix: vulnerabilidad hash md5 - teorico v1

--- db/__init__.py
+++ db/__init__.py
@@ -1,13 +1,16 @@
 import sqlite3
 import os
-import hashlib
+from werkzeug.security import generate_password_hash, check_password_hash
 
 DB_DIR = os.path.join(os.path.dirname(__file__), '..', 'db')
 USERS_DB = os.path.join(DB_DIR, 'users.db')
 DATA_DB = os.path.join(DB_DIR, 'data.db')
 
 def hash_password(password):
-    return hashlib.md5(password.encode()).hexdigest()
+    return generate_password_hash(password)
+
+def verify_password(stored_password, provided_password):
+    return check_password_hash(stored_password, provided_password)
 
 def ensure_users_db():
     # Primero, asegurarse de que las tablas de datos existen (empresas, comentarios)
